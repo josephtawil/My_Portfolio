aef0c03adf2c6c86dfb8e8159e2965b571e5c7a2
Joseph <jtawil2015@gmail.com>
2020-10-10 23:48:05 -0800

    added on click function to send button

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-$(document).ready(function() {
-    $("#linkedin").on("click",() =>{
-        window.location = "https://www.linkedin.com/in/joseph-tawil/" ;
-    });
-    $("#github").on("click",() =>{
-        window.location = "https://github.com/josephtawil?tab=repositories" ;
-    });
-    $("#resume").on("click",() =>{
-       location.href = "./styling/resume/MyResume.pdf";
-    });
+$(document).ready(function () {
+  $("#linkedin").on("click", () => {
+    window.location = "https://www.linkedin.com/in/joseph-tawil/";
+  });
+  $("#github").on("click", () => {
+    window.location = "https://github.com/josephtawil?tab=repositories";
+  });
+  $("#resume").on("click", () => {
+    location.href = "./styling/resume/MyResume.pdf";
+  });
 
-    $("#project1").on("click",()=>{
-        window.location = "https://astrology-app-astrologica.herokuapp.com/members";
-    });
+  $("#project1").on("click", () => {
+    window.location = "https://astrology-app-astrologica.herokuapp.com/members";
+  });
 
-    $("#project2").on("click",()=>{
-        window.location = "https://josephtawil.github.io/CSSearch/";
-    })
-    
-    $("#project3").on("click",()=>{
-        location.href="./soon.html";
-    })
+  $("#project2").on("click", () => {
+    window.location = "https://josephtawil.github.io/CSSearch/";
+  });
+
+  $("#project3").on("click", () => {
+    location.href = "./soon.html";
+  });
+
+  $("#btn").on("click", () => {
+    window.open("mailto:j.tawil2015@gmail.com");
+  });
 });
